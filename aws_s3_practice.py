7d5b375a2d762d93c459669c0d10e867a7e793d6
Ayan Ashkenov <ayan.ashkenov@gmail.com>
2023-02-18 19:39:27 -0500

    added code to upload an image; added that image for reference

--- aws_s3_practice.py
+++ aws_s3_practice.py
@@ -3,9 +3,11 @@ import boto3
 s3 = boto3.resource(
     service_name='s3',
     region_name='us-east-1',
-    aws_access_key_id='##############',
-    aws_secret_access_key='##############'
+    aws_access_key_id='', ### PUT ACCESS KEY HERE
+    aws_secret_access_key='' ### PUT SECRET KEY HERE
 )
 
 for bucket in s3.buckets.all():
     print(bucket.name)
+
+s3.Bucket('cs779').upload_file(Filename='av_vs_arsenal.jpeg', Key='av_vs_arsenal.jpeg')
