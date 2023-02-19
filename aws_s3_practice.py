14858438cd7f278f9844e395f1ff96a40039bbc6
Ayan Ashkenov <ayan.ashkenov@gmail.com>
2023-02-18 20:35:46 -0500

    added retrieving code from s3 to python

--- aws_s3_practice.py
+++ aws_s3_practice.py
@@ -1,4 +1,5 @@
 import boto3
+import pandas as pd
 
 s3 = boto3.resource(
     service_name='s3',
@@ -11,3 +12,7 @@ for bucket in s3.buckets.all():
     print(bucket.name)
 
 s3.Bucket('cs779').upload_file(Filename='av_vs_arsenal.jpeg', Key='av_vs_arsenal.jpeg')
+
+s3_object = s3.Bucket('cs779').Object('BostonHousing.csv').get()
+boston_housing_data = pd.read_csv(s3_object['Body'], index_col=0)
+print(len(boston_housing_data))
